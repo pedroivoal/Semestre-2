7c5df62edd8766b96b29df320fcf34c89adfab15
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2022-05-22 10:02:22 -0300

    .

--- CoDesing/js/js atividade/js/script.js
+++ CoDesing/js/js atividade/js/script.js
@@ -12,57 +12,33 @@ addEventListener('DOMContentLoaded', function(){
     lDescricao[0].innerHTML = 'Uma breve introdução à Programação Web'
     lDescricao[1].innerHTML = 'Criando interfaces amigáveis com HTML e CSS'
 
-    // Exercício 3
-    imgMode = document.querySelector('.img-mode')
-    imgMode.addEventListener('click', function(event){
-
-        imgMode = document.querySelector('.img-mode')
-
-        if (imgMode.src == "http://127.0.0.1:5500/js%20atividade/img/dark-mode-off.png"){
-            imgMode.src = "http://127.0.0.1:5500/js%20atividade/img/dark-mode-on.png"
-        } else {
-            imgMode.src = "http://127.0.0.1:5500/js%20atividade/img/dark-mode-off.png"
-        }
-
-    })
+    // Exercício 3 no final junto com o Exercício 6
 
     // Exercício 4
     selecionar = document.querySelectorAll('button') 
     selecionar[0].addEventListener('click', function(event){
 
-        borda = document.querySelectorAll('.card')
+        let borda = document.querySelectorAll('.card')
 
         if (borda[0].style.border == '0.01rem solid red'){
             borda[0].style.border = '0.01rem solid gray'
-            borda[1].style.border = '0.01rem solid gray'
             selecionar[0].innerHTML = 'Selecionar'
-            selecionar[1].innerHTML = 'Selecionar'
-        }else{
-            
+        }else{  
             borda[0].style.border = '0.01rem solid red'
-            borda[1].style.border = '0.01rem solid red'
             selecionar[0].innerHTML = 'Selecionado'
-            selecionar[1].innerHTML = 'Selecionado'
         }
-        
     })
     selecionar[1].addEventListener('click', function(event){
 
-        borda = document.querySelectorAll('.card')
+        let borda = document.querySelectorAll('.card')
 
         if (borda[1].style.border == '0.01rem solid red'){
-            borda[0].style.border = '0.01rem solid gray'
             borda[1].style.border = '0.01rem solid gray'
-            selecionar[0].innerHTML = 'Selecionar'
             selecionar[1].innerHTML = 'Selecionar'
-        }else{
-            
-            borda[0].style.border = '0.01rem solid red'
+        }else{   
             borda[1].style.border = '0.01rem solid red'
-            selecionar[0].innerHTML = 'Selecionado'
             selecionar[1].innerHTML = 'Selecionado'
         }
-        
     })
 
     // Exercício 5
@@ -71,5 +47,43 @@ addEventListener('DOMContentLoaded', function(){
     nCard = document.querySelectorAll('.card').length
     h1.innerHTML = 'Cursos Ativos: '+String(nCard)
     header.appendChild(h1)
-    
+
+    // Exercício 3 + Exercício 6
+    imgMode = document.querySelector('.img-mode')
+    imgMode.addEventListener('click', function(event){
+
+        body = document.querySelector('body')//Exercício 6
+        title = document.querySelector('.home-link')//Exercício 6
+        h5 = document.querySelectorAll('h5')//Exercício 6
+
+        imgMode = document.querySelector('.img-mode')
+
+        if (imgMode.src == "http://127.0.0.1:5500/js%20atividade/img/dark-mode-off.png"){
+            imgMode.src = "http://127.0.0.1:5500/js%20atividade/img/dark-mode-on.png"
+            body.style.background = '#2b2b2b'//Exercício 6
+            header.style.background = '#171010'//Exercício 6
+            h1.style.color = 'white'//Exercício 6
+            curso1.style.color = 'white'//Exercício 6
+            curso2.style.color = 'white'//Exercício 6
+            lDescricao[0].style.color = 'white'//Exercício 6
+            lDescricao[1].style.color = 'white'//Exercício 6
+            imgMode.style.background = '#423F3E'//Exercício 6
+            title.style.color = 'white'//Exercício 6
+            h5[0].style.color = 'white'//Exercício 6
+            h5[1].style.color = 'white'//Exercício 6
+        } else {
+            imgMode.src = "http://127.0.0.1:5500/js%20atividade/img/dark-mode-off.png"
+            body.style.background = 'white'//Exercício 6
+            header.style.background = 'whitesmoke'//Exercício 6
+            h1.style.color = 'black'//Exercício 6
+            curso1.style.color = 'black'//Exercício 6
+            curso2.style.color = 'black'//Exercício 6
+            lDescricao[0].style.color = 'black'//Exercício 6
+            lDescricao[1].style.color = 'black'//Exercício 6
+            imgMode.style.background = 'whitesmoke'//Exercício 6
+            title.style.color = 'gray'//Exercício 6
+            h5[0].style.color = 'black'//Exercício 6
+            h5[1].style.color = 'black'//Exercício 6
+        }
+    })
 })
